d75940915f666e6540fcdafc9dbb467f55d8a7bc
rasiq24 <bandayrasiq12911@gmail.com>
2026-02-12 22:39:45 +0530

    normal distribution

---
+++ probability/index.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+x = np.linspace(-10,10,1000)
+mu = 0
+sig = 1
+f = np.exp((-(x-mu)**2)/2*sig**2)/(np.sqrt(2*np.pi)*sig)
+
+plt.plot(x,f)
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.title("Normal Distribution")
+plt.grid(True)
+plt.show()
